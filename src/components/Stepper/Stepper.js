640f45483b08f2ed7bf57d0b8cdcd90b318ac450
kbuza <buza.work@gmail.com>
2020-09-25 17:22:33 +0300

    created Stepper component

---
+++ src/components/Stepper/Stepper.js
@@ -0,0 +1,23 @@
+import "./Stepper.css";
+import React from "react";
+import { Step } from "../Step/Step";
+
+export const Stepper = ({ steps, activeStep, onStepChange }) => {
+  const renderStep = (step, index, isActive) => (
+    <Step onClick={() => onStepChange(step)} isActive={isActive}>
+      {index + 1}
+    </Step>
+  );
+  const renderSteps = () => {
+    const activeStepIndex = steps.indexOf(activeStep);
+    return steps.map((step, index) => (
+      <li key={index} className="stepper__step-container">
+        {renderStep(step, index, activeStepIndex === index)}
+        {index !== steps.length - 1 ? (
+          <div className="stepper__separator" />
+        ) : null}
+      </li>
+    ));
+  };
+  return <ul className="stepper__container">{renderSteps()}</ul>;
+};
