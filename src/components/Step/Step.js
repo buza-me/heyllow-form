97fc52efe80332bdbb5a487227c72115d537bcb0
kbuza <buza.work@gmail.com>
2020-09-25 17:51:24 +0300

    fixed a typo

--- src/components/Step/Step.js
+++ src/components/Step/Step.js
@@ -4,7 +4,7 @@ import React from "react";
 export const Step = ({ children, isActive, className = "", onClick }) => (
   <div
     className={`step__container ${isActive ? "active" : ""}${className}`}
-    tabindex="0"
+    tabIndex="0"
     onClick={onClick}
     onKeyPress={(evt) => evt.key === "Enter" && onClick()}
   >
