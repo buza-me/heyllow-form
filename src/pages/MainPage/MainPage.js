ddddf402a2cc0518cbd38c858815abd4986b56cc
kbuza <buza.work@gmail.com>
2020-09-25 17:24:17 +0300

    added main page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,1 +1,43 @@
-import './MainPage.css';
+import "./MainPage.css";
+import React, { useContext, useState, useMemo, useRef } from "react";
+import { TextContentContext } from "../../providers";
+import { Stepper } from "../../components";
+import { MainPageForm } from "../../components";
+
+export const MainPage = () => {
+  const { getText } = useContext(TextContentContext);
+  const steps = useRef([
+    { content: null },
+    { content: null },
+    { content: <MainPageForm /> },
+  ]).current;
+  const [currentStep, setCurrentStep] = useState(steps[steps.length - 1]);
+
+  const header = useMemo(
+    () => (
+      <h1 className="main-page__container_header">
+        {getText("mainPage.header")}
+      </h1>
+    ),
+    [getText]
+  );
+  const stepper = useMemo(
+    () => (
+      <Stepper
+        steps={steps}
+        activeStep={currentStep}
+        onStepChange={setCurrentStep}
+      />
+    ),
+    [steps, currentStep]
+  );
+  return (
+    <section className="main-page__container">
+      {header}
+      <main className="main-page__content">
+        {stepper}
+        {currentStep && currentStep.content}
+      </main>
+    </section>
+  );
+};
